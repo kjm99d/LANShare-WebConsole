24991a43a842d0c058695f6a7b6783a2e3293262
KJM <kjm99d@naver.com>
2023-03-06 10:41:54 +0900

    ✨ 서비스 상태검증 로직 추가

--- src/components/ServiceSetting.vue
+++ src/components/ServiceSetting.vue
@@ -1,8 +1,30 @@
 <template>
   <v-sheet width="300" class="mx-auto">
-    
-      <v-text-field label="Service URL"></v-text-field>
-      <v-btn type="submit" block class="mt-2">Connection Test</v-btn>
-    
+
+    <v-text-field v-model="this.service_url" label="Service URL"></v-text-field>
+    <v-btn type="button" v-on:click="this.test_status()" block class="mt-2">Connection Test</v-btn>
+
   </v-sheet>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      service_url: "http://localhost:5004"
+    }
+  },
+  methods: {
+    test_status() {
+      axios.get(this.service_url + "/status")
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
